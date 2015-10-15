55c2deb25db2897a48fe7fb4e0cb081d2448e90e
Rémy HUBSCHER <rhubscher@mozilla.com>
2015-10-15 13:40:05 +0200

    First Kivy app.

---
+++ main.py
@@ -0,0 +1,33 @@
+import kivy
+kivy.require('1.1.9')
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+
+
+class LoginScreen(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text='User Name'))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text='password'))
+        self.password = TextInput(password=True, multiline=False)
+        self.add_widget(self.password)
+
+
+class MyApp(App):
+
+    def build(self):
+        layout = AnchorLayout(anchor_x='center', anchor_y='center')
+        layout.add_widget(LoginScreen(size_hint=(0.8, 0.1)))
+        return layout
+
+
+if __name__ == '__main__':
+    MyApp().run()
